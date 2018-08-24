62f50c6e2baac6983bc9d6284e5720c5aa403cad
Giustino Borzacchiello <giustinob@gmail.com>
2018-08-24 18:29:01 +0200

    Refactor tests to use dataProvider

--- php/01-tip-calculator/tests/TipCalculatorTest.php
+++ php/01-tip-calculator/tests/TipCalculatorTest.php
@@ -8,15 +8,28 @@ use PHPUnit\Framework\TestCase;
 
 class TipCalculatorTest extends TestCase
 {
-    public function test_CheckEverythingWorksTest()
+    /**
+     * @dataProvider tip_data_provider
+     * @param $bill
+     * @param $tipRate
+     * @param $tipAmount
+     * @param $totalAmount
+     */
+    public function test_CalculateTipAmountAndTotalAmountCorrectly($bill, $tipRate, $tipAmount, $totalAmount)
     {
-        $tc = new TipCalculator(200, 10);
-        $this->assertEquals(20, $tc->getTipAmount());
-        $this->assertEquals(220, $tc->getTotalAmount());
+        $tc = new TipCalculator($bill, $tipRate);
+        $this->assertEquals($tipAmount, $tc->getTipAmount());
+        $this->assertEquals($totalAmount, $tc->getTotalAmount());
+    }
 
-        $tc2 = new TipCalculator(11.25, 15);
-        $this->assertEquals(1.69, $tc2->getTipAmount());
-        $this->assertEquals(12.94, $tc2->getTotalAmount());
+    public function tip_data_provider()
+    {
+        return [
+            [200, 10, 20, 220],
+            [11.25, 15, 1.69, 12.94],
+            [0, 15, 0, 0],
+        ];
+
     }
 
 }
