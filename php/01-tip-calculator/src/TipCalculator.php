b734e0c0f8f2870c6e32d398516393abf93adf98
Giustino Borzacchiello <giustinob@gmail.com>
2018-08-24 18:25:18 +0200

    Calculate tip amount and bill total

--- php/01-tip-calculator/src/TipCalculator.php
+++ php/01-tip-calculator/src/TipCalculator.php
@@ -1,7 +1,30 @@
 <?php
+
 namespace Jubstuff\TipCalculator;
 
 
-class TipCalculator {
+class TipCalculator
+{
 
+    private $billAmount = 0;
+    private $tipRate = 0;
+
+    public function __construct($billAmount, $tipRate)
+    {
+        $this->billAmount = $billAmount;
+
+        $this->tipRate = $tipRate;
+    }
+
+    public function getTipAmount(): float
+    {
+        $percentage = $this->tipRate / 100;
+
+        return round($this->billAmount * $percentage, 2);
+    }
+
+    public function getTotalAmount(): float
+    {
+        return $this->billAmount + $this->getTipAmount();
+    }
 }
--- php/01-tip-calculator/tests/TipCalculatorTest.php
+++ php/01-tip-calculator/tests/TipCalculatorTest.php
@@ -6,11 +6,17 @@ namespace Jubstuff\TipCalculator\Test;
 use Jubstuff\TipCalculator\TipCalculator;
 use PHPUnit\Framework\TestCase;
 
-class TipCalculatorTest extends TestCase {
-	public function test_CheckEverythingWorksTest() {
-		$tc = new TipCalculator();
+class TipCalculatorTest extends TestCase
+{
+    public function test_CheckEverythingWorksTest()
+    {
+        $tc = new TipCalculator(200, 10);
+        $this->assertEquals(20, $tc->getTipAmount());
+        $this->assertEquals(220, $tc->getTotalAmount());
 
-		$this->assertInstanceOf( TipCalculator::class, $tc );
-	}
+        $tc2 = new TipCalculator(11.25, 15);
+        $this->assertEquals(1.69, $tc2->getTipAmount());
+        $this->assertEquals(12.94, $tc2->getTotalAmount());
+    }
 
 }
